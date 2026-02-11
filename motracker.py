d90b6b376bd955cff842a7358a77cd9f89941e06
JOVIIAN99 <masekow01@protonmail.com>
2026-02-11 14:15:11 +0200

    Update motracker.py

--- motracker.py
+++ motracker.py
@@ -1,1 +1,10 @@
+income = float(input("Enter your income: "))
+expenses = float(input("Enter your expenses: "))
+
+remaining = income - expenses
+
+print("------ Daily Summary ------")
+print("Income:", income)
+print("Expenses:", expenses)
+print("Remaining balance:", remaining)
 
